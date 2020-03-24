b5c63a11e486e324dafc26ad1fa4234d60282c76
TaufanP <tefanzprih@gmail.com>
2020-03-24 19:48:26 +0700

    Change URL

--- src/env.js
+++ src/env.js
@@ -1,2 +1,2 @@
-export const URL_ADDRESS = "http://localhost:8080/api/v1/"
-// export const URL_ADDRESS = "http://54.159.200.168:8081/api/v1/"
+// export const URL_ADDRESS = "http://localhost:8080/api/v1/"
+export const URL_ADDRESS = "http://54.159.200.168:8081/api/v1/"
